445beee543f6bfd4fc169c9ffed41795fcd0b648
Chadrackmbuyi01 <macbookpro@MacBooks-MacBook-Pro.local>
2023-08-29 03:52:32 +0200

    Added functionality for the next action

--- public/script/script.js
+++ public/script/script.js
@@ -1,11 +1,16 @@
 function cardFunctionality () {
     
     let action = document.getElementById("nameInput").value;
+    let action2 = document.getElementById("numberInput").value;
 
     if(action == null || action == ""){
         document.getElementById("message").innerHTML="field required";
         return false;
-    } else{
+    } else if(action2 == null || action2 == ""){
+        document.getElementById("message").innerHTML="field required";
+        return false;
+    }else{
     document.getElementById("nameOutput").innerHTML = action;
+    document.getElementById("numberOutput").innerHTML = action2;
     }
 }
