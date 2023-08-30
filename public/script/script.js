f2300a233139328bd69f4f41e1aefbc540748714
Chadrackmbuyi01 <macbookpro@MacBooks-MacBook-Pro.local>
2023-08-30 11:38:46 +0200

    Rendering completed for first both input textfields

--- public/script/script.js
+++ public/script/script.js
@@ -1,16 +1,31 @@
-function cardFunctionality () {
+nameInput.oninput = showValue;
+numberInput.oninput = showValue2;
     
-    let action = document.getElementById("nameInput").value;
-    let action2 = document.getElementById("numberInput").value;
-
-    if(action == null || action == ""){
-        document.getElementById("message").innerHTML="field required";
-        return false;
-    } else if(action2 == null || action2 == ""){
-        document.getElementById("message").innerHTML="field required";
-        return false;
-    }else{
-    document.getElementById("nameOutput").innerHTML = action;
-    document.getElementById("numberOutput").innerHTML = action2;
-    }
+function showValue(){
+    nameOutput.value = this.value;
 }
+function showValue2(){
+    numberOutput.value = this.value;
+}
+
+
+
+
+
+// function cardFunctionality () {
+
+//  let action = document.getElementById("nameInput").value;
+//      let action2 = document.getElementById("numberInput").value;
+
+//      if(action == null || action == ""){
+//          document.getElementById("message").innerHTML="field required";
+//         return false;
+//      } else if(action2 == null || action2 == ""){
+//          document.getElementById("message").innerHTML="field required";
+//          return false;
+//      }else{
+//      document.getElementById("nameOutput").innerHTML = action;
+//      document.getElementById("numberOutput").innerHTML = action2;
+//      }
+//  }
+    
